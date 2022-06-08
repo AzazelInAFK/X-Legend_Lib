4f9bd84a07c033b00af8813403eeb4b694739ce4
AzazelIsAFK <86533552+AzazelInAFK@users.noreply.github.com>
2022-06-08 11:44:53 -0300

    Update Error.h

--- include/Error.h
+++ include/Error.h
@@ -1,30 +1,65 @@
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Code produced by Azazel.										   //
-//You can contact me on Ragezone here "https://forum.ragezone.com/members/2000318605.html".		   //
-//I take no responsaiblity over this code or how it is used.						   //
-//This code was tested on Grand Fantasia files, it may or may not work on AK, EE or another X-Legend game. //
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 #pragma once
 
 #include <Windows.h>
-#include <direct.h>
-#include <string>
+#include <comdef.h>
 #include <sstream>
-#include <fstream>
 
-static HRESULT hr;
+static HRESULT g_shr;
 
-#define Log(Msg) Seraph::Error::_Log(Msg, __FILE__, __LINE__)
+#ifdef _DEBUG
+#define AssertC(Expr, Code) \
+    if(!Err(#Expr, Expr, __FILE__, __LINE__, Code)) return false
+#define Assert(Expr) \
+    if(!Err(#Expr, Expr, __FILE__, __LINE__)) return false
+#define AssertCM(Expr, Code, Message) \
+    if(!Err(#Expr, Expr, __FILE__, __LINE__, Code, Message)) return false
+#define AssertM(Expr, Message) \
+    if(!Err(#Expr, Expr, __FILE__, __LINE__, UINT32_MAX, Message)) return false
+#else
+#define AssertC(Expr, Code) \
+    if(!Err(Expr, Code)) return false
+#define Assert(Expr) \
+    if(!Err(Expr)) return false
+#define AssertM(Expr, Message) \
+    if(!Err(Expr, Code, UINT32_MAX, Message)) return false
+#define AssertCM(Expr, Code, Message) \
+    if(!Err(Expr, Code, Message)) return false
+#endif
 
-namespace Seraph {
-	void RecurseDirCreate(std::string FilePath);
-	namespace Error {
-		static unsigned short m_ErrorCount;
-		static std::stringstream m_ErrorMessage;
 
-		void Console(float Val);
-		bool _Log(std::string Msg, std::string File, int Line);
-		bool Throw();
-	};
+static bool Err(bool expr, HRESULT hr = UINT32_MAX, const char* pMsg = nullptr)
+{
+    if (!expr)
+    {
+        std::wstringstream wcErrDesc;
+        wcErrDesc << "Assert failed." << std::endl;
+        if (pMsg)
+            wcErrDesc << "Error description:\t\t" << pMsg << std::endl;
+        if (hr != UINT32_MAX) {
+            _com_error errbuffer(hr);
+            wcErrDesc << L"Code:\t" << errbuffer.ErrorMessage() << "\n\n";
+        }
+        MessageBoxW(nullptr, wcErrDesc.str().c_str(), nullptr, 0);
+    }
+    return expr;
+}
+
+static bool Err(const char* expr_str, bool expr, const char* file, int line, HRESULT hr = UINT32_MAX, const char* pMsg = nullptr)
+{
+    if (!expr)
+    {
+        std::wstringstream wcErrDesc;
+        wcErrDesc << "Assert failed." << std::endl;
+        if (pMsg)
+            wcErrDesc << "Error description:\t" << pMsg << std::endl;
+        if (hr != UINT32_MAX) {
+            _com_error errbuffer(hr);
+            wcErrDesc << "Code:\t\t" << errbuffer.ErrorMessage() << "\n\n";
+        }
+        wcErrDesc   << "Expression:\t" << expr_str << "\n"
+                    << "Source:\t\t" << file << ", line " << line << "\n";
+        if (MessageBoxW(nullptr, wcErrDesc.str().c_str(), nullptr, MB_OKCANCEL) == IDCANCEL)
+            return true;
+    }
+    return expr;
 }
